34bc8c28ba8364a1b997d5b567a0881aba5c9f61
chung567115 <1173572995@qq.com>
2016-05-11 09:07:23 +0800

    511

--- app/src/main/java/com/lenso/jixiangbao/activity/SplashActivity.java
+++ app/src/main/java/com/lenso/jixiangbao/activity/SplashActivity.java
@@ -1,7 +1,17 @@
 package com.lenso.jixiangbao.activity;
 
+import android.os.Bundle;
+
+import com.lenso.jixiangbao.R;
+
 /**
  * Created by king on 2016/5/10.
  */
 public class SplashActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
+
