bffef83329f5d386574cf3aef7d27f2afd6a0005
DoimicKing <jgm_king@outlook.com>
2016-05-11 15:49:33 +0800

    5.10

--- app/src/main/java/com/lenso/jixiangbao/activity/SplashActivity.java
+++ app/src/main/java/com/lenso/jixiangbao/activity/SplashActivity.java
@@ -2,20 +2,35 @@ package com.lenso.jixiangbao.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.lenso.jixiangbao.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by king on 2016/5/10.
  */
 public class SplashActivity extends BaseActivity {
+    @Bind(R.id.imageView)
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        init();
+        ButterKnife.bind(this);
+        imageView.setImageResource(R.mipmap.ic_launcher);
     }
-     private void init(){
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus)
+            init();
+    }
+
+    private void init() {
         Intent intent = new Intent(this, LaunchActivity.class);
         startActivity(intent);
         finish();
