cd10f3965a13f4edff289d9d3b7fef7139543d77
chung567115 <1173572995@qq.com>
2016-05-20 16:15:03 +0800

    HTML接口

--- app/src/main/java/com/lenso/jixiangbao/api/HTMLInterface.java
+++ app/src/main/java/com/lenso/jixiangbao/api/HTMLInterface.java
@@ -6,27 +6,28 @@ package com.lenso.jixiangbao.api;
 public class HTMLInterface {
     public static final String H5_URL = "h5_url";
     public static final String H5_TITLE = "h5_title";
+    public static final String DOMAIN = "http://meishusheng.len.so";
 
     /**
      * LoanFragment HTML 5 API
      */
-    public static final String LOAN = "http://meishusheng.len.so/assets/borrowdetail.html";
+    public static final String LOAN = DOMAIN + "/assets/borrowdetail.html";
 
     /**
      * MineFragment HTML 5 API
      */
-    public static final String USERTYPE = "http://meishusheng.len.so/assets/member/verified.html";
-    public static final String MESSAGE = "http://meishusheng.len.so/assets/member/message.html";
-    public static final String LJTY = "http://meishusheng.len.so/assets/experiencedetail.html";
-    public static final String TJYJ = "http://meishusheng.len.so/assets/member/recommend-list.html";
-    public static final String JF = "http://meishusheng.len.so/assets/member/mall.html";
-    public static final String TX = "http://meishusheng.len.so/assets/member/withdraw.html";
-    public static final String CZ = "http://meishusheng.len.so/assets/member/recharge.html";
+    public static final String USERTYPE = DOMAIN + "/assets/member/verified.html";
+    public static final String MESSAGE = DOMAIN + "/assets/member/message.html";
+    public static final String LJTY = DOMAIN + "/assets/experiencedetail.html";
+    public static final String TJYJ = DOMAIN + "/assets/member/recommend-list.html";
+    public static final String JF = DOMAIN + "/assets/member/mall.html";
+    public static final String TX = DOMAIN + "/assets/member/withdraw.html";
+    public static final String CZ = DOMAIN + "/assets/member/recharge.html";
 
-    public static final String ZJGK = "http://meishusheng.len.so/assets/member/income.html";
-    public static final String WDTZ = "http://meishusheng.len.so/assets/member/investment.html";
-    public static final String ZQZR = "http://meishusheng.len.so/assets/member/transfer.html";
-    public static final String TYB = "http://meishusheng.len.so/assets/member/experience-detail.html";
-    public static final String WDJK = "http://meishusheng.len.so/assets/member/loan-record.html";
-    public static final String ZHXX = "http://meishusheng.len.so/assets/member/account.html";
+    public static final String ZJGK = DOMAIN + "/assets/member/income.html";
+    public static final String WDTZ = DOMAIN + "/assets/member/investment.html";
+    public static final String ZQZR = DOMAIN + "/assets/member/transfer.html";
+    public static final String TYB = DOMAIN + "/assets/member/experience-detail.html";
+    public static final String WDJK = DOMAIN + "/assets/member/loan-record.html";
+    public static final String ZHXX = DOMAIN + "/assets/member/account.html";
 }
