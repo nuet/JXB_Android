2288753bceae36e27613f8b63432a7a610f5ac30
chung567115 <1173572995@qq.com>
2016-05-24 17:20:48 +0800

    JPush

--- app/src/main/java/com/lenso/jixiangbao/App.java
+++ app/src/main/java/com/lenso/jixiangbao/App.java
@@ -1,8 +1,11 @@
 package com.lenso.jixiangbao;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.lenso.jixiangbao.http.VolleyHttp;
+
+import cn.jpush.android.api.JPushInterface;
 
 /**
  * Created by king on 2016/5/19.
@@ -12,5 +15,10 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         VolleyHttp.getIntance().init(getApplicationContext());
+
+        JPushInterface.setDebugMode(true);//设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);// 初始化 JPush
+        Log.i("App->JPush","initialed");
+
     }
 }
